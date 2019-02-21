c83b09b14b6815d48f79076b16728fcf41fc60d6
Frederick-S <mao_xiaodan@hotmail.com>
2019-02-21 08:13:07 +0800

    Add benchmark statistic

--- benchmark_result.go
+++ benchmark_result.go
@@ -9,3 +9,19 @@ type BenchmarkResult struct {
 
 	StatisticBuckets []BenchmarkStatistic
 }
+
+func (benchmarkResult *BenchmarkResult) addStatistic(bucket int, benchmarkStatistic BenchmarkStatistic) {
+	if bucket > len(benchmarkResult.StatisticBuckets)-1 {
+		newStatisticBuckets := make([]BenchmarkStatistic, bucket+1)
+
+		copy(newStatisticBuckets, benchmarkResult.StatisticBuckets)
+
+		benchmarkResult.StatisticBuckets = newStatisticBuckets
+	}
+
+	statisticBucket := benchmarkResult.StatisticBuckets[bucket]
+	statisticBucket.count += benchmarkStatistic.count
+	statisticBucket.time += benchmarkStatistic.time
+
+	benchmarkResult.StatisticBuckets[bucket] = statisticBucket
+}
