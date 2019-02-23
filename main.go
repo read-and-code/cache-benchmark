029870a3fba6b8ddcb87cb505acfd18b983f56ee
Frederick-S <mao_xiaodan@hotmail.com>
2019-02-23 22:34:55 +0800

    Add run

--- main.go
+++ main.go
@@ -1,6 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/read-and-code/cache-benchmark/cache_client"
+	"time"
+)
+
+func run(cacheClient cache_client.CacheClient, cmd *cache_client.Cmd, benchmarkResult *BenchmarkResult) {
+	expectedValue := cmd.Value
+	startTime := time.Now()
+
+	cacheClient.Run(cmd)
+
+	duration := time.Now().Sub(startTime)
+	resultType := cmd.Name
+
+	if resultType == "get" {
+		if cmd.Value == "" {
+			resultType = "miss"
+		} else if cmd.Value != expectedValue {
+			panic(cmd)
+		}
+	}
+
+	benchmarkResult.addDuration(duration, resultType)
+}
 
 func main() {
 	fmt.Println("Hello")
