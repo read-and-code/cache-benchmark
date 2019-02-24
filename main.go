e44a30261961f2563c1fdde1b3a82371a5ca6e2b
Frederick-S <mao_xiaodan@hotmail.com>
2019-02-24 20:21:16 +0800

    Add pipeline

--- main.go
+++ main.go
@@ -26,6 +26,38 @@ func run(cacheClient cache_client.CacheClient, cmd *cache_client.Cmd, benchmarkR
 	benchmarkResult.addDuration(duration, resultType)
 }
 
+func pipeline(cacheClient cache_client.CacheClient, cmds []*cache_client.Cmd, benchmarkResult BenchmarkResult) {
+	expectedValues := make([]string, len(cmds))
+
+	for i, cmd := range cmds {
+		if cmd.Name == "get" {
+			expectedValues[i] = cmd.Value
+		}
+	}
+
+	startTime := time.Now()
+
+	cacheClient.PipelinedRun(cmds)
+
+	duration := time.Now().Sub(startTime)
+
+	for i, cmd := range cmds {
+		resultType := cmd.Name
+
+		if resultType == "get" {
+			if cmd.Value == "" {
+				resultType = "miss"
+			} else if cmd.Value != expectedValues[i] {
+				fmt.Println(expectedValues[i])
+
+				panic(cmd.Value)
+			}
+		}
+
+		benchmarkResult.addDuration(duration, resultType)
+	}
+}
+
 func main() {
 	fmt.Println("Hello")
 }
