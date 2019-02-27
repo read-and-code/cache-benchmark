1d2b9909ef4d867cdbd2811b4b4a557064a47c7f
Frederick-S <mao_xiaodan@hotmail.com>
2019-02-27 22:48:13 +0800

    Add operation

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/read-and-code/cache-benchmark/cache_client"
+	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -26,7 +28,7 @@ func run(cacheClient cache_client.CacheClient, cmd *cache_client.Cmd, benchmarkR
 	benchmarkResult.addDuration(duration, resultType)
 }
 
-func pipeline(cacheClient cache_client.CacheClient, cmds []*cache_client.Cmd, benchmarkResult BenchmarkResult) {
+func pipeline(cacheClient cache_client.CacheClient, cmds []*cache_client.Cmd, benchmarkResult *BenchmarkResult) {
 	expectedValues := make([]string, len(cmds))
 
 	for i, cmd := range cmds {
@@ -57,6 +59,58 @@ func pipeline(cacheClient cache_client.CacheClient, cmds []*cache_client.Cmd, be
 		benchmarkResult.addDuration(duration, resultType)
 	}
 }
+
+func operate(id, count int, channel chan *BenchmarkResult) {
+	cacheClient := cache_client.NewCacheClient(cacheClientType, serverAddress)
+	cmds := make([]*cache_client.Cmd, 0)
+	valuePrefix := strings.Repeat("a", valueSize)
+	benchmarkResult := &BenchmarkResult{0, 0, 0, make([]BenchmarkStatistic, 0)}
+
+	for i := 0; i < count; i++ {
+		var randomNumber int
+
+		if keyspaceLength > 0 {
+			randomNumber = rand.Intn(keyspaceLength)
+		} else {
+			randomNumber = id*count + i
+		}
+
+		key := fmt.Sprintf("%d", randomNumber)
+		value := fmt.Sprintf("%s%d", valuePrefix, randomNumber)
+		name := operation
+
+		if operation == "mixed" {
+			if rand.Intn(2) == 1 {
+				name = "set"
+			} else {
+				name = "get"
+			}
+		}
+
+		cmd := &cache_client.Cmd{Name: name, Key: key, Value: value, Error: nil}
+
+		if pipelineLength > 1 {
+			cmds = append(cmds, cmd)
+
+			if len(cmds) == pipelineLength {
+				pipeline(cacheClient, cmds, benchmarkResult)
+
+				cmds = make([]*cache_client.Cmd, 0)
+			}
+		} else {
+			run(cacheClient, cmd, benchmarkResult)
+		}
+	}
+
+	if len(cmds) != 0 {
+		pipeline(cacheClient, cmds, benchmarkResult)
+	}
+
+	channel <- benchmarkResult
+}
+
+var cacheClientType, serverAddress, operation string
+var totalRequests, valueSize, threads, keyspaceLength, pipelineLength int
 
 func main() {
 	fmt.Println("Hello")
